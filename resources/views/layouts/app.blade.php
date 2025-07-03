2fa7ff85acd83c4729f58c6db6d1fc8a242b062e
victor@walkiriaapps.com <victor@walkiriaapps.com>
2025-07-03 19:02:39 +0200

    google verifier

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
-
+    <meta name="google-site-verification" content="h6GdrP1wEh-zmAIW2X5mp9PViXCwUU_kOLAqqcU46mM" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     {{-- Meta dinámico --}}
